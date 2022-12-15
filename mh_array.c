d058e0c1c98e1f51028607caafc36d630eee89ec
zhuzhiwen0607 <zhuzhiwen0607@qq.com>
2022-12-15 14:08:14 +0800

    add array

---
+++ mh_array.c
@@ -0,0 +1,56 @@
+#include <assert.h>
+#include "mh_array.h"
+#include "mh_mm.h"
+#include "mh_error.h"
+
+
+static mh_result_t mh_array_init(mh_array_p array, size);
+static mh_result_t mh_array_deinit(mh_array_p array);
+
+mh_result_t mh_array_new(mh_array_p* array, size)
+{
+    assert(array);
+
+    mh_result_t ret = mh_malloc(array, size);
+    if (MH_OK != ret)
+        return ret;
+
+    return mh_array_init(*array, size);
+
+}
+
+mh_result_t mh_array_destroy(mh_array_p* array)
+{
+    assert(array);
+
+    mh_array_deinit(*array);
+
+    mh_free(array);
+}
+
+static mh_result_t mh_array_init(mh_array_p array, size)
+{
+    assert(array);
+
+    mh_result_t ret = mh_malloc(&(array->start), size);
+    if (MH_OK != ret)
+        return ret;
+
+    array->p = array->start;
+    array->end = array->start + size + 1;   // *end point to buffer_end+1
+    array->byte_offset = 0;
+
+    return MH_OK;
+}
+
+static mh_result_t mh_array_deinit(mh_array_p array)
+{
+    assert(array);
+
+    mh_free(&(array->start));
+
+    array->start = NULL;
+    array->end = NULL;
+    array->p = NULL;
+    array->byte_offset = 0;
+}
--- mh_array.h
+++ mh_array.h
@@ -1,4 +1,18 @@
 #ifndef MH_ARRAY_H
 #define MH_ARRAY_H
 
+#include "mh_type.h"
+
+typedef struct
+{
+    mh_uint8_t *start;       // start pos of array elements
+    mh_uint8_t *end;
+    mh_uint8_t *p;          // current base offset
+    mh_uint8_t byte_offset; // bits offset in byte which p point
+//    mh_int32_t size;  // size of array elements
+}mh_array_t, *mh_array_p;
+
+mh_result_t mh_array_new(mh_array_p* array, size);
+mh_result_t mh_array_destroy(mh_array_p* array);
+
 #endif // MH_ARRAY_H

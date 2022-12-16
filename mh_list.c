48dbd054b3e9fd738201eff5943f1cd34fd06a75
zhuzhiwen0607 <zhuzhiwen0607@qq.com>
2022-12-16 10:48:31 +0800

    mh_list operations

--- mh_list.c
+++ mh_list.c
@@ -1,2 +1,101 @@
-
+#include <assert.h>
 #include "mh_list.h"
+#include "mh_mm.h"
+#include "mh_type.h"
+#include "mh_error.h"
+#include "mh_log.h"
+
+mh_result_t mh_list_new(mh_list_p *list)
+{
+    mh_result_t ret = mh_malloc(list, sizeof(mh_list_t));
+    if (MH_OK != ret)
+        return ret;
+
+    (*list)->prev = NULL;
+    (*list)->next = NULL;
+
+    return MH_OK;
+}
+
+mh_result_t mh_list_destroy(mh_list_p *list)
+{
+    assert(*list);
+
+    if ((*list)->prev || (*list)->next)
+        mh_error("list->previous or list->next is not NULL, current list maybe broken!");
+
+    mh_free(list);
+
+    return MH_OK;
+}
+
+mh_result_t mh_list_push_back(mh_list_p list, mh_list_p x)
+{
+    assert(list);
+
+    mh_list_p p = list;
+
+    while (!tail(p))
+    {
+        p = next(p);
+    }
+
+    p->next = x;
+    x->prev = p;
+    x->next = NULL;
+
+    return MH_OK;
+}
+
+mh_result_t mh_list_push_front(mh_list_p list, mh_list_p x)
+{
+    assert(list);
+
+    mh_list_p p = list;
+
+    while (!header(p))
+    {
+        p = previous(p);
+    }
+
+    x->next = p;
+    p->prev = x;
+    x->prev = NULL;
+
+    return MH_OK;
+}
+
+mh_result_t mh_list_insert(mh_list_p list, mh_list_p x, mh_int32_t i)
+{
+
+}
+
+mh_result_t mh_list_delete(mh_list_p list, mh_int32_t i)
+{
+
+}
+
+mh_list_p mh_list_pop_back(mh_list_p list)
+{
+
+}
+
+mh_list_p mh_list_pop_front(mh_list_p list)
+{
+
+}
+
+mh_list_p mh_list_back(mh_list_p list)
+{
+
+}
+
+mh_list_p mh_list_front(mh_list_p list)
+{
+
+}
+
+mh_list_p mh_list_search(mh_list_p list, mh_int32_t i)
+{
+
+}
--- mh_list.h
+++ mh_list.h
@@ -7,12 +7,63 @@
 
 struct mh_list_s
 {
-    struct mh_list_s *previous;
+    struct mh_list_s *prev;
     struct mh_list_s *next;
 };
 
 typedef struct mh_list_s mh_list_t;
 typedef struct mh_list_s* mh_list_p;
+
+
+mh_result_t mh_list_new(mh_list_p list);
+mh_result_t mh_list_destroy(mh_list_p list);
+
+mh_result_t mh_list_push_back(mh_list_p list, mh_list_p x);
+mh_result_t mh_list_push_front(mh_list_p list, mh_list_p x);
+mh_result_t mh_list_insert(mh_list_p list, mh_list_p x, mh_int32_t i);
+mh_result_t mh_list_delete(mh_list_p list, mh_int32_t i);
+mh_list_p mh_list_pop_back(mh_list_p list);
+mh_list_p mh_list_pop_front(mh_list_p list);
+mh_list_p mh_list_back(mh_list_p list);
+mh_list_p mh_list_front(mh_list_p list);
+mh_list_p mh_list_search(mh_list_p list, mh_int32_t i);
+
+static inline mh_bool_t header(mh_list_p list);
+static inline mh_bool_t tail(mh_list_p list);
+static inline mh_list_p previous(mh_list_p list);
+static inline mh_list_p next(mh_list_p list);
+
+static inline mh_bool_t header(mh_list_p list)
+{
+    assert(list);
+    if (!list->prev)
+        return mh_true;
+
+    return mh_false;
+}
+
+static inline mh_bool_t tail(mh_list_p list)
+{
+    assert(list);
+    if (!list->next)
+        return mh_true;
+
+    return mh_false;
+}
+
+static inline mh_list_p previous(mh_list_p list)
+{
+    assert(list);
+    return list->prev;
+}
+
+static inline mh_list_p next(mh_list_p list)
+{
+    assert(list);
+    return list->next;
+}
+
+
 
 
 #endif // MH_LIST_H
